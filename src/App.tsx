7905bad16c500ddd439418f9d33625220d8ce27e
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-04-21 22:07:06 +0300

    Update CI/CD title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>🚀 CI/CD works!</h1>
+      <h1>CI/CD works!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
