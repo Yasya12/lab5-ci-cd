8ba2f6738e62fedec3a89ce94439ba0bf0b5a06e
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-04-21 22:34:54 +0300

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR 111111
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        It was hard to make this work, but I did it! <br />
       </p>
     </>
   )
