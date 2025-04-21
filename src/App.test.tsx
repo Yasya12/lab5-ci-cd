d70500799bd1e3c917fcd2126d5fe1f51982d538
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-04-21 16:42:15 +0300

    Fix test: update expected title

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 describe('App', () => {
   it('renders headline', () => {
     render(<App />);
-    const headline = screen.getByText(/Vite \+ React/i);
+    const headline = screen.getByText(/CI\/CD works/i);
     expect(headline).toBeInTheDocument();
   });
 });
